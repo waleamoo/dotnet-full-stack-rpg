67ebc2923e07e805a935f239fdec17def029f5b8
Amoo Wale <amoowale@gmail.com>
2022-12-27 09:17:36 +0100

    Complete fullstack CRUD

---
+++ SuperHero.UI/src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { SuperHero } from './models/super-hero';
+import { SuperHeroService } from './services/super-hero.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'SuperHero.UI';
+  heroes: SuperHero[] = [];
+  heroToEdit?: SuperHero;
+
+  constructor(private superHeroServices: SuperHeroService) {}
+
+  ngOnInit() : void {
+    // this.heroes = this.superHeroServices.getSuperHeroes();
+    // console.log(this.heroes);
+    this.superHeroServices
+    .getSuperHeroes()
+    .subscribe((result: SuperHero[]) => (this.heroes = result));
+  }
+
+  updateHeroList(heroes: SuperHero[]){
+    this.heroes = heroes;
+  }
+
+  initNewHero(){
+    this.heroToEdit = new SuperHero();
+  }
+
+  editHero(hero: SuperHero){
+    this.heroToEdit = hero;
+  }
+}
---
+++ SuperHeroAPI/SuperHeroAPI/Program.cs
@@ -0,0 +1,41 @@
+global using SuperHeroAPI.Data;
+global using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+// add the db context 
+
+
+// add cors
+builder.Services.AddCors(options => options.AddPolicy(name: "SuperHeroOrigins",
+    policy =>
+    {
+        policy.WithOrigins("http://localhost:4200").AllowAnyMethod().AllowAnyHeader();
+    }));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// use cors 
+app.UseCors("SuperHeroOrigins");
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
